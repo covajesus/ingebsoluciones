2902e63d9eb6c9f1a598dcd45c88349328b55771
jisjesuscova <jesuscova@jisparking.com>
2023-07-10 10:08:37 -0400

    cambios

--- app/Http/Controllers/Api/DteController.php
+++ app/Http/Controllers/Api/DteController.php
@@ -20,13 +20,9 @@ class DteController extends Controller
     public function index()
     {
         $branch_offices = BranchOffice::all();
-        print($branch_offices);
-        die();
         foreach($branch_offices as $branch_office) 
         {
-            echo $branch_office->branch_office_id;
-            die();
-            $cashiers = Cashier::where('branch_office_id', $branch_office->branch_office_id)->get();
+            $cashiers = Cashier::where('branch_office_id', $branch_office->id)->get();
 
             $date = date('Y-m-d', strtotime('-5 days', strtotime(date('Y-m-d'))));
 
@@ -36,15 +32,15 @@ class DteController extends Controller
                         ->selectRaw('SUM(c.cash_amount) as cash_amount, SUM(c.card_amount) as card_amount, COUNT(*) as quantity, DATE(c.created_at) as date')
                         ->groupBy(DB::raw('DATE(c.created_at)'))
                         ->whereDate('created_at', '>=', $date)
-                        ->where('c.branch_office_id', $branch_office->branch_office_id)
-                        ->where('c.cashier_id', $cashier->cashier_id)
+                        ->where('c.branch_office_id', $branch_office->id)
+                        ->where('c.cashier_id', $cashier->id)
                         ->get();
 
                 foreach($dtes as $dte) {
-                    $collection_qty = Collection::where('branch_office_id', $branch_office->branch_office_id)->where('cashier_id', $cashier->cashier_id)->whereRaw('DATE(created_at) = "'. $dte->date .'"')->count();
+                    $collection_qty = Collection::where('branch_office_id', $branch_office->id)->where('cashier_id', $cashier->id)->whereRaw('DATE(created_at) = "'. $dte->date .'"')->count();
 
                     if($collection_qty > 0) {
-                        $collection = Collection::where('branch_office_id', $branch_office->branch_office_id)->where('cashier_id', $cashier->cashier_id)->whereRaw('DATE(created_at) = "'. $dte->date .'"')->first();
+                        $collection = Collection::where('branch_office_id', $branch_office->id)->where('cashier_id', $cashier->id)->whereRaw('DATE(created_at) = "'. $dte->date .'"')->first();
                         $cash_amount = $collection->cash_amount;
                         $card_amount = $collection->card_amount;
                         $ticket_number = $collection->quantity;
@@ -68,8 +64,8 @@ class DteController extends Controller
                         $cash_amount = $collection->cash_amount;
                         $card_amount = $collection->card_amount;
                         $ticket_number = $collection->quantity;
-                        $collection->branch_office_id = $branch_office->branch_office_id;
-                        $collection->cashier_id = $cashier->cashier_id;
+                        $collection->branch_office_id = $branch_office->id;
+                        $collection->cashier_id = $cashier->id;
                         $collection->cash_amount = $dte->cash_amount;
                         $collection->card_amount = $dte->card_amount;
                         $collection->quantity = $dte->quantity;
