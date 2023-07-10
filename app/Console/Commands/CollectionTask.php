fcb91ea7aa0798a84558e94ef49ad4d08a4a7960
jisjesuscova <jesuscova@jisparking.com>
2023-07-10 07:59:36 -0400

    cambios

--- app/Console/Commands/CollectionTask.php
+++ app/Console/Commands/CollectionTask.php
@@ -25,6 +25,68 @@ class CollectionTask extends Command
      */
     public function handle()
     {
-        //
+        $branch_offices = BranchOffice::all();
+
+        foreach($branch_offices as $branch_office) 
+        {
+            $cashiers = Cashier::where('branch_office_id', $branch_office->branch_office_id)->get();
+
+            $date = date('Y-m-d', strtotime('-5 days', strtotime(date('Y-m-d'))));
+
+            foreach($cashiers as $cashier) {
+                $dtes = Dte::from('dtes as c')
+                        ->selectRaw('SUM(c.cash_amount) as cash_amount, SUM(c.card_amount) as card_amount, COUNT(*) as quantity, DATE(c.created_at) as date')
+                        ->groupBy(DB::raw('DATE(c.created_at)'))
+                        ->whereDate('created_at', '>=', $date)
+                        ->where('c.branch_office_id', $branch_office->branch_office_id)
+                        ->where('c.cashier_id', $cashier->cashier_id)
+                        ->get();
+
+                foreach($dtes as $dte) {
+                    $collection_qty = Collection::where('branch_office_id', $branch_office->branch_office_id)->where('cashier_id', $cashier->cashier_id)->whereRaw('DATE(created_at) = "'. $dte->date .'"')->count();
+
+                    if($collection_qty > 0) {
+                        $collection = Collection::where('branch_office_id', $branch_office->branch_office_id)->where('cashier_id', $cashier->cashier_id)->whereRaw('DATE(created_at) = "'. $dte->date .'"')->first();
+                        $cash_amount = $collection->cash_amount;
+                        $card_amount = $collection->card_amount;
+                        $ticket_number = $collection->quantity;
+                        $old_amount = $collection->cash_amount + $collection->card_amount;
+                        $new_amount = $dte->cash_amount + $dte->card_amount;
+                        $collection->cash_amount = $dte->cash_amount;
+                        $collection->card_amount = $dte->card_amount;
+                        $collection->quantity = $dte->quantity;
+                        $tz = 'America/Santiago';
+                        $timestamp = time();
+                        $dt = new DateTime("now", new \DateTimeZone($tz));
+                        $dt->setTimestamp($timestamp);
+                        $datetime = $dt->format('Y-m-d H:i:s');
+                        $collection->updated_at = $datetime;
+                        
+                        if($old_amount < $new_amount) {
+                            $collection->save();
+                        }
+                    } else {
+                        $collection = new Collection;
+                        $cash_amount = $collection->cash_amount;
+                        $card_amount = $collection->card_amount;
+                        $ticket_number = $collection->quantity;
+                        $collection->branch_office_id = $branch_office->branch_office_id;
+                        $collection->cashier_id = $cashier->cashier_id;
+                        $collection->cash_amount = $dte->cash_amount;
+                        $collection->card_amount = $dte->card_amount;
+                        $collection->quantity = $dte->quantity;
+                        $collection->created_at = $dte->date .' 00:00:00';
+                        $tz = 'America/Santiago';
+                        $timestamp = time();
+                        $dt = new DateTime("now", new \DateTimeZone($tz)); //first argument "must" be a string
+                        $dt->setTimestamp($timestamp); //adjust the object to correct timestamp
+                        $datetime = $dt->format('Y-m-d H:i:s');
+
+                        $collection->updated_at = $datetime;
+                        $collection->save();
+                    }
+                }
+            }
+        }
     }
 }
