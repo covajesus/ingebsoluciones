4938a0265c8529a8252641e5a653d349f169c9a2
jisjesuscova <jesuscova@jisparking.com>
2023-07-19 07:32:56 -0400

    cambios

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/account';
 
     public const ADMINISTRATOR_HOME = '/administrator';
 
